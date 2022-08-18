285c0df88c10929797a5349131d4a8b109e95336
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-08-18 17:09:06 -0300

    Requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system"><Title headline="Planetas" /></div>;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {
+          Planets.map((p) => {
+            const { name } = p;
+            const n = name;
+            return <PlanetCard key={ n } planetName={ n } planetImage={ p.image } />;
+          })
+        }
+      </div>
+    );
   }
 }
 
